e5e380a3b9cc2d78b9142ebbbb468a975e8fea8b
Dayeane <dayeane85@gmail.com>
2021-11-22 20:23:21 -0800

    commit tree f1a27c05de8a05e1ead5e17902bc2832435c4088

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+# this is a dog
+class Dog
+  def sit
+    puts 'The Dog is sitting'
+  end
+
+  def bark
+    puts 'Woof!'
+  end
+end
+
+fido = Dog.new
+fido.bark
+fido.sit
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+# this is a person 
+class Person
+  def talk
+    puts 'Hello World!'
+  end
+
+  def walk
+    puts 'The Person is walking'
+  end
+end
+
+salvatore = Person.new
+salvatore.talk
+salvatore.walk
